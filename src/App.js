59ead15267510790bcc7b84309029c7904788fde
joel vilca Alania <joevil23tjq@gmail.com>
2024-01-28 09:06:28 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./pages/Homepage";
+import Plans from "./pages/Plans";
+import Resumen from "./pages/Resumen";
+import Layout from "./shared/Layout";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <Layout>
+              <Home />
+            </Layout>
+          }
+        />
+        <Route
+          path="/plans"
+          element={
+            <Layout>
+              <Plans />
+            </Layout>
+          }
+        />
+        <Route
+          path="/resumen"
+          element={
+            <Layout>
+              <Resumen />
+            </Layout>
+          }
+        />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
